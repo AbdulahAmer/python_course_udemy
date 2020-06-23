51c15d1a51fe7e60efb7ce764fa4fc134ab1d12d
Paul Torres <pntorres1542@gmail.com>
2020-06-22 20:13:52 -0400

    this is the first exercise

---
+++ EX01_km2miles.py
@@ -0,0 +1,3 @@
+print("This program will convert kilometers to miles.")
+km = float(input("Please enter the number of kilometers to be converted:"))
+print(km, "kilometers equals", round((km/1.609344),2),"miles.")
